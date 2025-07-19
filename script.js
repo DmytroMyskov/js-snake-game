9569b1f5606a9869fb9028ac07c210073e5864c6
Dmytro <coulheyman@gmail.com>
2025-07-19 16:10:28 +0300

    Refactor game loop and update functions for improved animation and movement handling

--- script.js
+++ script.js
@@ -2,21 +2,22 @@ const canvas = document.getElementById('board');
 const ctx = canvas.getContext('2d');
 const width = canvas.width;
 const height = canvas.height;
+let speed = 2; //cells per second
+let stepInterval = 1000 / speed;
 const boardSize = 20;
 const cellSize = width / boardSize;
 const food = [2, 2];
 const snake = [[4, 4], [4, 5], [4, 6], [5, 6], [6, 6]];
+let direction = 'right';
+const movements = {
+  right: [1, 0],
+  down: [0, 1],
+  left: [-1, 0],
+  up: [0, -1]
+}
 
-drawGrid(boardSize, boardSize);
-drawFood(...food);
-drawSnake(...snake);
-
-food[0] = Math.floor(Math.random() * boardSize) + 1;
-food[1] = Math.floor(Math.random() * boardSize) + 1;
-
-drawGrid(boardSize, boardSize);
-drawFood(...food);
-drawSnake(...snake);
+render();
+requestAnimationFrame(animate);
 
 function drawGrid(columnCount, rowCount) {
   ctx.fillStyle = 'white';
@@ -38,5 +39,44 @@ function drawSnake(...positions) {
   for (const [x, y] of positions) {
     ctx.fillStyle = 'green';
     ctx.fillRect((x - 1) * cellSize + 1, (y - 1) * cellSize + 1, cellSize - 2, cellSize - 2);
+  }
+}
+
+function render() {
+  drawGrid(boardSize, boardSize);
+  drawFood(...food);
+  drawSnake(...snake);
+}
+
+function animate(now) {
+  animate.previous ||= now
+  const dt = now - animate.previous
+  animate.previous = now
+  update(dt)
+  render()
+  requestAnimationFrame(animate)
+}
+
+function update(dt) {
+  update.leftover ||= {}
+  {
+    update.leftover.food ||= 0
+    const time = dt + update.leftover.food
+    const shift = Math.floor(time / stepInterval)
+    update.leftover.food = time % stepInterval
+    food[1] += shift
+  }
+  {
+    update.leftover.snake ||= 0
+    const time = dt + update.leftover.snake
+    const shift = Math.floor(time / stepInterval)
+    update.leftover.snake = time % stepInterval
+    for (let i = 0; i < shift; i++) {
+      const [x, y] = snake.at(-1)
+      const movement = movements[direction]
+      const [dx, dy] = movement
+      snake.push([x + dx, y + dy])
+      snake.shift()
+    }
   }
 }
