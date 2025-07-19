5c1ac32ca518bfa95ff0226403af1b09ea5afa8b
Dmytro <coulheyman@gmail.com>
2025-07-19 17:18:35 +0300

    Enhance snake movement logic and game mechanics for improved gameplay experience

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('board');
 const ctx = canvas.getContext('2d');
 const width = canvas.width;
 const height = canvas.height;
-let speed = 2; //cells per second
+let speed = 5; //cells per second
 let stepInterval = 1000 / speed;
 const boardSize = 20;
 const cellSize = width / boardSize;
@@ -10,6 +10,7 @@ const food = [2, 2];
 const snake = [[4, 4], [4, 5], [4, 6], [5, 6], [6, 6]];
 let direction = 'right';
 let moved = false;
+let taskId;
 const movements = {
   right: [1, 0],
   down: [0, 1],
@@ -26,7 +27,7 @@ const opposite = {
 onkeydown = handleKeys
 
 render();
-requestAnimationFrame(animate);
+taskId = requestAnimationFrame(animate);
 
 function drawGrid(columnCount, rowCount) {
   ctx.fillStyle = 'white';
@@ -61,9 +62,9 @@ function animate(now) {
   animate.previous ||= now
   const dt = now - animate.previous
   animate.previous = now
+  taskId = requestAnimationFrame(animate)
   update(dt)
   render()
-  requestAnimationFrame(animate)
 }
 
 function update(dt) {
@@ -77,9 +78,48 @@ function update(dt) {
     const [x, y] = snake.at(-1)
     const movement = movements[direction]
     const [dx, dy] = movement
-    snake.push([x + dx, y + dy])
-    snake.shift()
+    const head = [x + dx, y + dy];
+    const report = recon(head)
+    switch (report) {
+      case "empty":
+        snake.push(head)
+        snake.shift()
+        break;
+
+      case "food":
+        snake.push(head)
+        placeFood()
+        break;
+
+      case "wall":
+      case "snake":
+        endGame()
+    }
     moved = true
+  }
+}
+
+function endGame() {
+  cancelAnimationFrame(taskId)
+  alert('Game Over')
+}
+
+function placeFood() {
+  do {
+    food[0] = Math.floor(Math.random() * boardSize) + 1
+    food[1] = Math.floor(Math.random() * boardSize) + 1
+  } while (recon(food) === 'snake')
+}
+
+function recon([x, y]) {
+  if (x < 1 || x > boardSize || y < 1 || y > boardSize) {
+    return 'wall'
+  } else if (snake.some((part) => part[0] === x && part[1] === y)) {
+    return 'snake'
+  } else if (food[0] === x && food[1] === y) {
+    return 'food'
+  } else {
+    return 'empty'
   }
 }
 
